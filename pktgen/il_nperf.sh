b15ef1bd0cc9405cb9dcd37fdb785b7801be8593
張耀文 <hadeschang0322.cs04@g2.nctu.edu.tw>
2021-04-30 08:32:25 +0000

    Fixed that color of latency word is same as black bg

--- pktgen/il_nperf.sh
+++ pktgen/il_nperf.sh
@@ -6,18 +6,18 @@ if [[ $1 == "-g" ]] || [[ $1 == "--generator" ]]; then
 	s1u_cores="[$((${cores[0]}+1)):$((${cores[0]}+2))].0"
 	cmd="./app/x86_64-native-linuxapp-gcc/pktgen -l $core_list -n 4 --proc-type auto  \
 		--log-level 7 --socket-mem $NUMA0_MEM,$NUMA1_MEM --file-prefix s1u_pg -w $s1u_port -- -t -T     \
-		-N -m $s1u_cores -f ./autotest/input.txt -f themes/black-yellow.theme"
+		-N -m $s1u_cores -f ./autotest/input.txt -f themes/black_bg-yellow.theme"
 elif [[ $1 == "-r" ]] || [[ $1 == "--responder" ]]; then
 	#sgi_cores="[$(( ${cores[0]}+5 ))-$(( ${cores[0]}+6)):$(( ${cores[0]}+7 ))-$(( ${cores[0]}+8))].0"
 	sgi_cores="[$((${cores[0]}+3)):$((${cores[0]}+4))].0"
 	cmd="./app/x86_64-native-linuxapp-gcc/pktgen -l $core_list -n 4 --proc-type auto  \
 		--log-level 7 --socket-mem $NUMA0_MEM,$NUMA1_MEM --file-prefix sgi_pg -w $sgi_port -- -r -T     \
-		-N -m $sgi_cores -f ./autotest/input.txt -f themes/black-yellow.theme"
+		-N -m $sgi_cores -f ./autotest/input.txt -f themes/black_bg-yellow.theme"
 elif [[ $1 == "-rs" ]] || [[ $1 == "--responder_as_reflector" ]]; then
 	sgi_cores="$(( ${cores[0]}+3 )).0"
 	cmd="./app/x86_64-native-linuxapp-gcc/pktgen -l $core_list -n 4 --proc-type auto  \
 		--log-level 7 --socket-mem $NUMA0_MEM,$NUMA1_MEM --file-prefix sgi_ref_pg -w $sgi_port -- -x -T     \
-		-N -m $sgi_cores -f ./autotest/input.txt -f themes/black-yellow.theme"
+		-N -m $sgi_cores -f ./autotest/input.txt -f themes/black_bg-yellow.theme"
 else
 	echo -e "Usage: ./il_nperf.sh <option>\n"
 	echo "options:"
